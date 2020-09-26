b482ed8b88d6663d645d27aaeda29eb963fcf521
Umut Tufanoglu <28development@gmail.com>
2020-09-26 17:46:31 +0200

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h1>{{ number }}</h1>
+    <button @click="incrementNumber">increment</button>
+    <button @click="decrementNumber">increment</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+  setup() {
+    const number = ref(0);
+    const incrementNumber = () => number.value++;
+    const decrementNumber = () => number.value--;
+
+    return { number, incrementNumber, decrementNumber };
+  }
+});
+</script>
